86319d0f1992787321d031608ee64341d4c1975f
NikitaRathore01 <nikitarathore99772523@gmail.com>
2026-06-24 22:26:47 +0530

    order and payment implementation

--- src/main/java/com/firstsmile/daoimpl/OrdersDAOImpl.java
+++ src/main/java/com/firstsmile/daoimpl/OrdersDAOImpl.java
@@ -1,33 +1,107 @@
 package com.firstsmile.daoimpl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.firstsmile.connection.Connector;
 import com.firstsmile.dao.OrdersDAO;
 import com.firstsmile.dto.Orders;
 
 public class OrdersDAOImpl implements OrdersDAO {
 
+	 private Connection con;
+	    public OrdersDAOImpl() {
+	    	   this.con=Connector.requestConnection();
+	    }
+	
 	@Override
 	public void addOrders(Orders order) {
-		// TODO Auto-generated method stub
+	String query = "insert into orders values(0,sysDate(),sysDate(),?,?)";
+	try {
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		ps.setInt(1, order.getUid());
+		ps.setInt(2, order.getPayment_id());
+		
+		ps.executeUpdate();
+		
+	
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
 		
 	}
 
 	@Override
 	public Orders getOrderById(Integer order_id) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select * from orders where order_id = ?";
+		Orders o = null;
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, order_id);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				o = new Orders();
+				o.setOrder_id(rs.getInt("order_id"));
+				o.setOrder_date(rs.getString("order_date"));
+				o.setDelivery_date(rs.getString("delivery_date"));
+				o.setUid(rs.getInt("uid"));
+				o.setPayment_id(rs.getInt("payment_id"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return o;
 	}
 
 	@Override
 	public List<Orders> getAllOrders() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Orders> olist = new ArrayList<>();
+		Orders o = null;
+		
+		String query = "select * from Orders";
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				o = new Orders();
+				o.setOrder_id(rs.getInt("order_id"));
+				o.setOrder_date(rs.getString("order_date"));
+				o.setDelivery_date(rs.getString("delivery_date"));
+				o.setUid(rs.getInt("uid"));
+				o.setPayment_id(rs.getInt("payment_id"));
+				
+				olist.add(o);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return olist;
 	}
 
 	@Override
 	public void deleteOrder(Integer order_id) {
-		// TODO Auto-generated method stub
+		String query = "delete from orders where order_id = ?";
+		
+		try {
+			PreparedStatement ps= con.prepareStatement(query);
+			ps.setInt(1, order_id);
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
--- src/main/java/com/firstsmile/daoimpl/ProductDAOImpl.java
+++ src/main/java/com/firstsmile/daoimpl/ProductDAOImpl.java
@@ -1,46 +1,164 @@
 package com.firstsmile.daoimpl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.firstsmile.connection.Connector;
 import com.firstsmile.dao.ProductDAO;
 import com.firstsmile.dto.Product;
 
-public class ProductDAOImpl implements ProductDAO {
 
+public class ProductDAOImpl implements ProductDAO {
+	 private Connection con;
+	    public ProductDAOImpl() {
+	    	   this.con=Connector.requestConnection();
+	    }
+	
 	@Override
 	public void addProduct(Product product) {
-		// TODO Auto-generated method stub
+		String query = "insert into product values(0,?,?,?)";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			
+			ps.setString(1,product.getProduct_name());
+			ps.setDouble(2, product.getProduct_price());
+			ps.setInt(3, product.getImage_id());
+			
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public Product getProductById(Integer product_id) {
-		// TODO Auto-generated method stub
-		return null;
+	String query = "select * from product where product_id = ?";
+	Product p = null;
+	
+	try {
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		ps.setInt(1, product_id);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			p = new Product();
+			p.setProduct_name(rs.getString("product_name"));
+			p.setProduct_price(rs.getDouble("product_price"));
+			p.setImage_id(rs.getInt("image_id"));
+		}
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} 
+	
+		return p;
 	}
 
 	@Override
 	public List<Product> getAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> plist = new ArrayList<>();
+		Product p = null;
+		
+		String query = "select * from product";
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				p = new Product();
+			    
+				p.setProduct_id(rs.getInt("product_id"));
+				p.setProduct_name(rs.getString("product_name"));
+				p.setProduct_price(rs.getDouble("product_price"));
+				p.setImage_id(rs.getInt("image_id"));
+				
+				plist.add(p);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+		return plist;
 	}
 
 	@Override
 	public void updateProduct(Product product) {
-		// TODO Auto-generated method stub
+		String query = "update product set product_name=?, product_price=? , image_id=? where id=?";
+		
+		try {
+			PreparedStatement ps = con.prepareCall(query);
+			
+			ps.setString(1, product.getProduct_name());
+			ps.setDouble(2, product.getProduct_price());
+			ps.setInt(3, product.getImage_id());
+			ps.setInt(4, product.getProduct_id());
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void deleteProduct(Integer productId) {
-		// TODO Auto-generated method stub
+		String query = "delete from product where product_id =?";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, productId);
+			
+			ps.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 	}
 
 	@Override
 	public List<Product> getProductByName(String product_name) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> plist = new ArrayList<>();
+		Product p = null;
+		
+		String query = "select * from product where product_name=?";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1, product_name);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				p = new Product();
+				p.setProduct_id(rs.getInt("product_id"));
+				p.setProduct_price(rs.getDouble("product_price"));
+				p.setImage_id(rs.getInt("image_id"));
+				
+				plist.add(p); 
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return plist;
 	}
 
 }
